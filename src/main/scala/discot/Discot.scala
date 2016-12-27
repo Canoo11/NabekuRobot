8647d63efdfca25cb977319c6a33f7afbffb0af1
Canoo <canoo11kit@gmail.com>
2016-12-28 01:16:42 +0900

    Add Listener register

--- src/main/scala/discot/Discot.scala
+++ src/main/scala/discot/Discot.scala
@@ -1,5 +1,14 @@
-object discot {
-  def main(args: Array[String]): Unit = {
-    
-  }
+package discot
+
+import sx.blah.discord.api._
+import discot.EventHandler._
+
+class Discot(val token: String) {
+  val client = new ClientBuilder().withToken(token).login()
+
+  println("discot instantiated")
+
+  client.getDispatcher.registerListener(MessageReceiveHandler)
+  client.getDispatcher.registerListener(MentionHandler)
+
 }
