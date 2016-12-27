c80128337c54d7df72666ec65f19ad6736abc8f4
Canoo <canoo11kit@gmail.com>
2016-12-28 01:11:14 +0900

    Add EventHandler MessageReceived and Mention

---
+++ src/main/scala/discot/EventHandler/MentionedHandler.scala
@@ -0,0 +1,18 @@
+package discot.EventHandler
+
+import sx.blah.discord.api.events.IListener
+import sx.blah.discord.handle.impl.events.MentionEvent
+import sx.blah.discord.handle.obj.IMessage
+
+
+object MentionHandler extends IListener[MentionEvent] {
+  def handle(event: MentionEvent) = {
+    val message = event.getMessage
+    val c = message.getContent.split(" ")(1)
+
+    c match{
+      case bonfire if c.equals("!bon") => message.reply("fire")
+      case _ => 
+    }
+  }
+}
---
+++ src/main/scala/discot/EventHandler/MessageReceiveHandler.scala
@@ -0,0 +1,18 @@
+package discot.EventHandler
+
+import sx.blah.discord.api.events.IListener
+import sx.blah.discord.handle.impl.events.MessageReceivedEvent
+
+
+object MessageReceiveHandler extends IListener[MessageReceivedEvent] {
+  final val watchChannel = "bot"
+
+  def handle(event: MessageReceivedEvent) = {
+    val message = event.getMessage
+    val channelName = message.getChannel.getName
+    val c = message.getContent
+
+    if(channelName.equals(watchChannel)) println(channelName + ":" + c)
+  }
+}
+
