3090c665f1eae4fb6bd79f2929754f8288dbd5cc
Canoo <canoo11kit@gmail.com>
2017-01-01 13:23:42 +0900

    Change name startup class

---
+++ src/main/scala/discot/Startup.scala
@@ -0,0 +1,14 @@
+package discot
+
+import sx.blah.discord.util._
+
+object Startup extends App{
+  val token = io.Source.fromFile("Token").mkString.stripLineEnd
+
+  try{
+   new Discot(token)
+  }catch{
+   case e:DiscordException => println("Bad token")
+   case e:Exception => println("Something happen")
+  }
+}
